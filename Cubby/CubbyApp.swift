ec00c2ab77940dea8ab6d493f9ff97e0d0910d90
LukasMystic <lukasstanley42@gmail.com>
2026-07-02 13:53:57 +0700

    Initial Commit

---
+++ Cubby/CubbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubbyApp.swift
+//  Cubby
+//
+//  Created by Stanley Pratama Teguh on 02/07/26.
+//
+
+import SwiftUI
+
+@main
+struct CubbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
