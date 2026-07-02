5b81693b072569e681d70b562e16b87d6e1966e4
VaxSie28 <mrbluesgt1@gmail.com>
2026-07-02 16:52:54 +0700

    feat: added background image "LPimage", added title "Learn Boundaries", added "Play" button, and added fonts "FreckleFace-Regular" and "Slackey-Regular"

--- Cubby/ContentView.swift
+++ Cubby/ContentView.swift
@@ -6,20 +6,32 @@
 //
 
 import SwiftUI
-import SpriteKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Stanley Ganteng")
+        ZStack() {
+            Image("LPimage")
+            VStack(){
+                Text("Learn\nBoundaries!")
+
+                    .font(.custom("FreckleFace-Regular", size: 128))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.white)
+                
+                Button("Play") {
+                }
+                .font(.custom("Slackey-Regular", size: 48))
+                .foregroundColor(Color(red: 255/255, green: 141/255, blue: 40/255))
+                .padding(.horizontal, 120)
+                .padding(.vertical, 24)
+                .background(.white.opacity(0.65), in: Capsule())
+                
+            }
         }
-        .padding()
     }
 }
+    
 
 #Preview {
     ContentView()
-}
+    }
